5eb0d0994aa5cf3622f9090f08baddd5b8e17aef
Taka2401 <tkht2401@gmail.com>
2021-08-16 12:56:01 +0900

    三項演算子

--- main.js
+++ main.js
@@ -1,17 +1,5 @@
-const signal = 'blue';
+const score = 80;
 
-if (signal === 'red') {
-  console.log('止まれ');
-} else if (signal === 'yellow') {
-  console.log('一旦停止');
-} else {
-  console.log('進め');
-};
+const comment = score > 80 ? 'good' : 'not good';
 
-const speed = 60;
-
-if (signal === 'blue') {
-  if (speed >= 60) {
-    console.log('スピード違反です');
-  }
-};
+console.log(comment);
