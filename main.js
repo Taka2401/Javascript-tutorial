395a9f4210aba335ab09d2e1ae6ef135391d860c
Taka2401 <tkht2401@gmail.com>
2021-08-16 12:49:11 +0900

    if文 条件分岐

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
-const member = {
-  'honda': {
-    height: 170,
-    hobby: 'サッカー'
-  },
-  'kagawa': {
-    height: 190,
-    hobby: 'バスケ'
-  }
-}
+const signal = 'blue';
 
-console.log(member.kagawa.height);
+if (signal === 'red') {
+  console.log('止まれ');
+} else if (signal === 'yellow') {
+  console.log('一旦停止');
+} else {
+  console.log('進め');
+};
+
+const speed = 60;
+
+if (signal === 'blue') {
+  if (speed >= 60) {
+    console.log('スピード違反です');
+  }
+};
