4f7ab72f473fa22c915a33dccea2cbbfea542a03
Taka2401 <tkht2401@gmail.com>
2021-08-16 15:18:04 +0900

    定義関数

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-const scores = [10, 20, 30];
 
-for ( score of scores ) {
-  console.log(score);
+
+function score(number1, number2) {
+  let number3 = number1 + number2;
+  return number3;
 }
+
+const number3 = score(3, 4);
+console.log(number3);
