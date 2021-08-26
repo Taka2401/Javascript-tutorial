fe3645e068dd8849bf3a413a9f98b89bc5df1368
Taka2401 <tkht2401@gmail.com>
2021-08-26 20:13:42 +0900

    定義関数

---
+++ javascript基礎/定義関数①/main.js
@@ -0,0 +1,19 @@
+// インプット引数無し
+// アウトプットなし
+
+function test() {
+  console.log('テスト');
+}
+
+test();
+
+// インプット引数あり
+// アウトプット戻り値なし
+
+const comment = 'コメント';
+
+function getComment(string) {
+  console.log(string);
+}
+
+getComment(comment);
