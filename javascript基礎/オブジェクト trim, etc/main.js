fecedaea15a5c264f0eb737bb29bafb53bbcfd3a
Taka2401 <tkht2401@gmail.com>
2021-08-26 21:06:15 +0900

    trim etc

--- javascript基礎/オブジェクト trim, etc/main.js
+++ javascript基礎/オブジェクト trim, etc/main.js
@@ -1,15 +1,17 @@
-const greeting = '  Hello world   ';
+// trim(); 前後の空白をなくす
 
+const greeting = '  Hello world   ';
 console.log(greeting);
 
 const greeting_2 = '  Hello world   ';
-
 console.log(greeting_2.trim());
 
-const name = 'tanaka';
+// substring(); 文字を切り取る(start, 直前の文字まで)
 
+const name = 'tanaka';
 console.log(name.substring(0, 4));
 
-const test = 'テストです';
+// replace(); 文字を置き換える(before, after);
 
+const test = 'テストです';
 console.log(test.replace('テスト', 'ファイト'));
