1ff11323e162e08eed934fe50913af09a1967a5a
Taka2401 <tkht2401@gmail.com>
2021-08-26 21:02:32 +0900

    trim etc

---
+++ javascript基礎/オブジェクト concat/main.js
@@ -0,0 +1,6 @@
+const test = 'テスト';
+const test_2 = 'です';
+
+console.log(test.concat(test_2));
+
+// concat(足したい文字);
---
+++ javascript基礎/オブジェクト trim, etc/main.js
@@ -0,0 +1,15 @@
+const greeting = '  Hello world   ';
+
+console.log(greeting);
+
+const greeting_2 = '  Hello world   ';
+
+console.log(greeting_2.trim());
+
+const name = 'tanaka';
+
+console.log(name.substring(0, 4));
+
+const test = 'テストです';
+
+console.log(test.replace('テスト', 'ファイト'));
