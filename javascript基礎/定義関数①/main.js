1f450ae5a6a23757e66c6756a5fe1837bce385ee
Taka2401 <tkht2401@gmail.com>
2021-08-26 20:29:51 +0900

    引数、戻り値あり

--- javascript基礎/定義関数①/main.js
+++ javascript基礎/定義関数①/main.js
@@ -17,3 +17,52 @@ function getComment(string) {
 }
 
 getComment(comment);
+
+// インプット引数なし
+// アウトプット戻り値あり
+
+function numberOfComment() {
+  return 5;
+}
+
+console.log(numberOfComment());
+
+const NumberOfComment = numberOfComment();
+
+console.log(NumberOfComment);
+
+// インプット 二つ
+// アウトプット戻り値あり
+
+function sumPrice(int1, int2) {
+  let int3 = int1 + int2;
+  return int3;
+};
+
+const total = sumPrice(4, 10);
+
+console.log(total);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
